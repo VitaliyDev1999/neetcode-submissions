cee52cdcaa384f25eb90ce43872cb687144ffbc8
Vitalii Deineha <34566305+VitaliyDev1999@users.noreply.github.com>
2026-04-18 19:24:36 +0200

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = piles.Max();
+        int res = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long  hRequired = 0;
+
+            foreach (int pile in piles) {
+                hRequired += (int)Math.Ceiling((double)pile / mid); 
+            }
+
+            if (hRequired <= h)
+            {   
+                res = mid;
+                right = mid - 1;
+                
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
