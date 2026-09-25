67a3b17672b04151bff0b3a3806cdfc1e4f4f9cb
Vitalii Deineha <34566305+VitaliyDev1999@users.noreply.github.com>
2026-09-25 18:49:14 +0200

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int maxReach = 0;
+        for(int i = 0;i < nums.Length;i++) {
+            if(i > maxReach)
+                return false;
+            if(i + nums[i] > maxReach)
+                maxReach = i + nums[i];
+        }
+        return true;
+    }
+}
